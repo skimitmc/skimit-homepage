dc6ffe61d8eb35c686580fbe9067cbbd419ede63
AnnaNo8 <102014391+AnnaNo8@users.noreply.github.com>
2023-01-20 03:19:28 +0000

    Deploying to gh-pages from @ skimitmc/skimit-docs-zh@ccbee0acff4e5e72036aa7bedac2c98ef3a6085d 🚀

--- assets/mermaid-mindmap.esm.min-d53be93d.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./app-a6907ea3.js";import"./framework-de7b8a75.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
