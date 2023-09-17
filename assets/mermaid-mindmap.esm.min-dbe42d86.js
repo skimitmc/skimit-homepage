5d2075311f53625071cdb8e2c40a072f86c7eaa9
Glucy-2 <Glucy-2@outlook.com>
2023-09-17 05:29:49 +0000

    Deploying to gh-pages from @ skimitmc/skimit-homepage@74d5bd0018545afa426caba423bb7f0d2f0356e8 🚀

---
+++ assets/mermaid-mindmap.esm.min-dbe42d86.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-538d9ede.js";import"./framework-de7b8a75.js";const t="mindmap",i=a=>a.match(/^\s*mindmap/)!==null,o=async()=>{const{diagram:a}=await r(()=>import("./diagram-definition.0faef4c2-4dda171c.js"),[]);return{id:t,diagram:a}},e={id:t,detector:i,loader:o};export{e as default};
